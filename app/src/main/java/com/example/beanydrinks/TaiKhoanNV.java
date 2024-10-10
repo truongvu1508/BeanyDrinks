c6a959bde1302ab6eacc195e9887bef6275ea937
Manh Hung <quangmanhhhungle@gmail.com>
2024-10-10 16:35:09 +0700

    chuyen tran

---
+++ app/src/main/java/com/example/beanydrinks/TaiKhoanNV.java
@@ -0,0 +1,20 @@
+package com.example.beanydrinks;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TaiKhoanNV extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tai_khoan_n_v, container, false);
+    }
+}
