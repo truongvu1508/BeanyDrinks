1ed67f186acfa30b183612739afd89d155a70510
Manh Hung <quangmanhhhungle@gmail.com>
2024-10-10 20:24:29 +0700

    doiMatKhau

--- app/src/main/java/com/example/beanydrinks/DoiMatKhauFragment.java
+++ app/src/main/java/com/example/beanydrinks/DoiMatKhauFragment.java
@@ -2,27 +2,18 @@ package com.example.beanydrinks;
 
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 
-public class TaiKhoanNV extends Fragment {
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
+public class DoiMatKhauFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tai_khoan_n_v, container, false);
+        return inflater.inflate(R.layout.fragment_doi_mat_khau, container, false);
     }
 }
