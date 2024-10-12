13e19a28966e552c5097e6a2e1dc4628c982f429
dothanhbao1805 <dothanhbao42@gmail.com>
2024-10-12 17:36:29 +0700

    bao_update_6UI

--- app/src/main/java/com/example/beanydrinks/MainActivity.java
+++ app/src/main/java/com/example/beanydrinks/MainActivity.java
@@ -1,24 +1,20 @@
 package com.example.beanydrinks;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_main); // Layout này sẽ chứa fragment
+
+        // Kiểm tra xem fragment đã được thêm vào chưa để tránh thêm lại trên xoay
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new orderban_nv()) // Sử dụng Fragment mới tạo
+                    .commit();
+        }
     }
 }
