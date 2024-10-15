cfda7249f503eb396889a68cc6805284819fdefe
truongvu1508 <ntv150804@gmail.com>
2024-10-15 21:30:19 +0700

    update event click

--- app/src/main/java/com/example/beanydrinks/orderban_nv.java
+++ app/src/main/java/com/example/beanydrinks/orderban_nv.java
@@ -14,8 +14,8 @@ public class orderban_nv extends AppCompatActivity {
         setContentView(R.layout.orderban_nv);  // Thiết lập layout cho Activity
 
         // Tìm button_AddMon và thiết lập sự kiện nhấn
-        Button button8 = findViewById(R.id.button_AddMon);
-        button8.setOnClickListener(new View.OnClickListener() {
+        Button btnAddMon = findViewById(R.id.button_AddMon);
+        btnAddMon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Tạo Intent để mở them_mon_cho_ban Activity
--- app/src/main/java/com/example/beanydrinks/them_mon_cho_ban.java
+++ app/src/main/java/com/example/beanydrinks/them_mon_cho_ban.java
@@ -3,6 +3,7 @@ package com.example.beanydrinks;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,15 +14,24 @@ public class them_mon_cho_ban extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.them_mon_cho_ban); // Gắn layout them_mon_cho_ban
 
-        // Tìm imageButton11 trong layout
-        ImageButton imageButton11 = findViewById(R.id.imageButton11);
-        imageButton11.setOnClickListener(new View.OnClickListener() {
+        // Xử lý sự kiện nút "Back"
+        ImageButton btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                // Tạo Intent để quay lại MainActivity và hiển thị orderban_nv
-//                Intent intent = new Intent(them_mon_cho_ban.this, MainActivity.class);
-//                intent.putExtra("showOrderBanNv", true); // Truyền dữ liệu để biết cần hiển thị lại Fragment orderban_nv
-//                startActivity(intent);
+                // Sử dụng finish() để quay lại màn hình orderban_nv
+                finish();
+            }
+        });
+
+        // Xử lý sự kiện nút "Đồng ý"
+        Button btnDongY = findViewById(R.id.button_dongy);
+        btnDongY.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Tạo Intent để chuyển đến màn hình thanhtoan_nv
+                Intent intent = new Intent(them_mon_cho_ban.this, thanhtoan_nvActivity.class);
+                startActivity(intent);
             }
         });
     }
