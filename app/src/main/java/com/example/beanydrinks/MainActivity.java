7bfbe8fb2d5be30acc97072366b770890d074978
dothanhbao1805 <dothanhbao42@gmail.com>
2024-10-15 13:54:22 +0700

    bao_update

--- app/src/main/java/com/example/beanydrinks/MainActivity.java
+++ app/src/main/java/com/example/beanydrinks/MainActivity.java
@@ -9,12 +9,5 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Khởi chạy thanhtoan_nvActivity
-        Intent intent = new Intent(MainActivity.this, thanhtoan_nvActivity.class);
-        startActivity(intent);
-
-        // Kết thúc MainActivity để không quay lại màn hình này nữa
-        finish();
     }
 }
