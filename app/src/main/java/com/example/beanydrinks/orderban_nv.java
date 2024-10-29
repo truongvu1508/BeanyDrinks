144b05cdf6e202fb425df0caa6c9672bf2aa650d
Manh Hung <quangmanhhhungle@gmail.com>
2024-10-29 20:37:42 +0700

    code su kien them tt khach va button back

--- app/src/main/java/com/example/beanydrinks/orderban_nv.java
+++ app/src/main/java/com/example/beanydrinks/orderban_nv.java
@@ -13,6 +13,17 @@ public class orderban_nv extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.orderban_nv);  // Thiết lập layout cho Activity
 
+
+        View themttkhach = findViewById(R.id.themttkhach);
+        themttkhach.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(orderban_nv.this,themthongtinkhachhang.class);
+                startActivity(intent);
+            }
+        });
+
+
         // Tìm button_AddMon và thiết lập sự kiện nhấn
         Button btnAddMon = findViewById(R.id.button_AddMon);
         btnAddMon.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/beanydrinks/themthongtinkhachhang.java
+++ app/src/main/java/com/example/beanydrinks/themthongtinkhachhang.java
@@ -1,6 +1,10 @@
 package com.example.beanydrinks;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +19,16 @@ public class themthongtinkhachhang extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_them_thong_tin_khach_hang);
+
+        ImageButton btnbackthemttkh = findViewById(R.id.btnbackthemttkh);
+        btnbackthemttkh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(themthongtinkhachhang.this , orderban_nv.class);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
