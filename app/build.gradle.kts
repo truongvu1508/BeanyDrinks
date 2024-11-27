da060ceffe96938897212bf9222c296a7cee8cc5
dothanhbao1805 <dothanhbao42@gmail.com>
2024-11-27 16:48:23 +0700

    bao_update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,6 @@ dependencies {
     implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation("com.android.volley:volley:1.2.1")
+
 }
---
+++ app/src/main/java/com/example/beanydrinks/ultil/Server.java
@@ -0,0 +1,6 @@
+package com.example.beanydrinks.ultil;
+
+public class Server {
+    public static String localhost ="192.168.100.208:8080";
+    public static String DuongDanNhanVien= "http://192.168.100.208:8080/server/getNhanVien.php";
+}
