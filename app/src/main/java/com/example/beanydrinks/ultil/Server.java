aa55c88034d727db945815697dba255f41d4b406
dothanhbao1805 <dothanhbao42@gmail.com>
2024-11-28 17:10:47 +0700

    bao_update

--- app/src/main/java/com/example/beanydrinks/ultil/Server.java
+++ app/src/main/java/com/example/beanydrinks/ultil/Server.java
@@ -1,11 +1,8 @@
 package com.example.beanydrinks.ultil;
 
 public class Server {
-<<<<<<< HEAD
+
     public static String localhost ="172.16.0.98:8080";
     public static String DuongDanNhanVien= "http://172.16.0.98:8080/server/getNhanVien.php";
-=======
-    public static String localhost ="192.168.1.37:8080";
-    public static String DuongDanNhanVien= "http://192.168.1.37:8080/server/getNhanVien.php";
->>>>>>> master
+
 }
