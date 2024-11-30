a9a4dc960aae5892cc154d2a04ff9c7823d73b04
Manh Hung <quangmanhhhungle@gmail.com>
2024-11-30 12:35:21 +0700

    lehung

--- app/src/main/java/com/example/beanydrinks/ultil/Server.java
+++ app/src/main/java/com/example/beanydrinks/ultil/Server.java
@@ -2,7 +2,7 @@ package com.example.beanydrinks.ultil;
 
 public class Server {
 
-    public static String localhost ="192.168.110.224:8080";
-    public static String DuongDanNhanVien= "http://192.168.110.224:8080/server/getNhanVien.php";
-    public static String DuongDanBan= "http://192.168.110.224:8080/server/getBan.php";
+    public static String localhost ="172.16.2.0:8080";
+    public static String DuongDanNhanVien= "http://172.16.2.0:8080/server/getNhanVien.php";
+    public static String DuongDanBan= "http://172.16.2.0:8080/server/getBan.php";
 }
