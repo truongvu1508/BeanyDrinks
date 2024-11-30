ac49bcbaa71f505754a7530af009338ebc5acb64
truongvu1508 <ntv150804@gmail.com>
2024-11-30 12:52:49 +0700

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,5 +52,5 @@ dependencies {
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation("com.android.volley:volley:1.2.1")
     implementation("com.squareup.picasso:picasso:2.71828")
-
+    implementation("com.karumi:dexter:6.2.3")
 }
